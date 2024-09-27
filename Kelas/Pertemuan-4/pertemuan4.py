39fe7a1b8b3a3745ab1e42b8addd0d7a15c53a84
SeiSendai <fathir.rahman775@gmail.com>
2024-09-27 14:59:51 +0800

    Add files via upload

---
+++ Kelas/Pertemuan-4/pertemuan4.py
@@ -0,0 +1,75 @@
+# ulang = 10
+# for i in range(ulang):
+    # print("perulangan ke-"+str(i)+"kali")
+    # print(f"perulangan ke--{i} kali")
+
+# for i in range(1, 11):
+#     print(f"perulangan ke--{i} kali")
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#     print(i)
+
+# print(simpam[2])
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print(f"{i} * {j} = {i * j}")
+#     print()
+
+# for i in range(1, 6, 2):
+#     for j in range(1, 6, 2):
+#         print(f"{i} * {j} = {i * j}")
+#     print()
+
+# for i in range(10, -1, -1):
+#     print(f"anak ayam turun ke--{i}")
+
+# ulang = "ya"
+# hitung = 0
+# while ulang == "ya":
+#     print(f"perulangan ke {hitung}")
+#     ulang = input("apakah anda masih ingin mengulang?")
+#     hitung +=1
+# print("perulangan selesai")
+
+# hitung = 1
+# while True:
+#     print(f"perulangan ke{hitung}")
+#     ulang =input("Apakah anda masih ingin mengulang?")\
+#     hitung +=1
+# print("perulangan selesai")
+
+# x = 0
+# while x < 5:
+#     print(x)
+#     x+=1
+
+# hitung = 1
+# while True:
+#     print(f"perulangan ke {hitung}")
+#     ulang = input("apakah anda masih ingin mengulang?")
+#     if ulang == "tidak":
+#         break
+#     hitung +=1
+# print(f"Total perulangan: {hitung}")
+
+# for i in range(10):
+#     if i % 2 == 0:
+#         continue
+#     if i == 5:
+#         break
+#     print(i)
+
+# un = "admin"
+# pw = "admin123"
+# salah = 0
+
+# while salah < 3:
+#     username = input("masukkan username: ")
+#     password = input("masukkan password: ")
+#     if un == username and pw == password:
+#         print("login berhasil")
+#         break
+#     else:
+#         print("login gagal")
