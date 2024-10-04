ade4e9e5679b509f7b0e92ff6fe530727f1d5977
SeiSendai <fathir.rahman775@gmail.com>
2024-10-04 15:23:55 +0800

    Add files via upload

---
+++ Kelas/Pertemuan-5/pertemuan5.py
@@ -0,0 +1,93 @@
+# list=["a",
+#       2,
+#       True,
+#       [1,2,3]]
+
+# print(list[-1][2])
+
+# tas = ["buku",32,True,3.14,["IF"24]]
+
+# # for i in tas:
+# #     print (i)
+# print(len(tas))
+
+# for i in range(len(tas)):
+#     print(tas[i])
+
+# matkul=["kalkulus","fisdas","pti"]
+# print(matkul)
+
+# # matkul.append("sastra mesin")
+# # print(matkul)
+# # matkul.insert=(1,"sastra mesin")
+# # print(matkul)
+
+# matkul[1]="fisika dasar"
+# print(matkul)
+
+# # del matkul [1]
+# # print(matkul)
+
+# buang = matkul.pop(1)
+# print(matkul)
+
+# print(f"Variable buang = {buang}")
+
+# prodi = ["informatika", "sistem informasi", "teknik arsitektur", "teknik lingkungan",
+# "teknik pertambangan", "teknik elektro",
+# "teknik geologi", "teknik kimia"]
+
+# print(prodi[0:10:2], "\n", prodi)
+
+# pyrochar = ["hu tao", "klee", "xiangling"]
+# hydrochar = ["furina", "nevuliette", "lapu lapu"]
+
+# party = pyrochar + hydrochar
+# print(pyrochar, "\n")
+# print(hydrochar, "\n")
+# print(party)
+
+# print(pyrochar*3)
+
+# barang = [["sepatu", "kaus kakhi", "sarung"],
+#           ["pulpen", "pensil", "laptop"]]
+# # print(barang[1][1])
+
+# # print(barang[-2][-1])
+
+# for i in barang:
+#     for j in i:
+#         print(j)
+
+# perabotan = []
+# print(perabotan)
+# perabotan.append("meja")
+# print(perabotan)
+
+# nama = "aldi", "daffa", "arishyi"
+
+# print(nama)
+# nama.append("rava")
+# print(nama)
+
+# barang1 = ["sepatu", "kaus kakhi", "sarung"]
+# barang2 = ["pulpen", "pensil", "laptop"]
+
+# barangtotal =[barang1, barang2]
+# print(barangtotal)
+
+# mahasiswa = (69, "informatika", "2209106044", "Aldy septian")
+# absen, prodi, nim. nama = mahasiswa
+
+# print(absen)
+# print(prodi)
+# print(nim)
+# print(nama)
+
+# mahasiswa = (69, "informatika", "2209106044", "Aldy septian")
+# print(mahasiswa)
+# mahasiswa= list(mahasiswa)
+# mahasiswa[2]="2409106076"
+
+# mahasiswa=tuple(mahasiswa)
+# print(mahasiswa)
