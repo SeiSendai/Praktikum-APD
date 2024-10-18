eec6445b97ee4611358b5159a0629f2c245b0cdb
SeiSendai <fathir.rahman775@gmail.com>
2024-10-18 14:57:00 +0800

    Add files via upload

---
+++ Kelas/Pertemuan-7/pertemuan7.py
@@ -0,0 +1,85 @@
+# def aku_fungsi ():
+#     print("hello world")
+
+# aku_fungsi()
+
+# def tambah(a,b):
+#     hasil = a+b
+
+#     print(hasil)
+
+# tambah(10,2)
+
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return hasil
+
+# print(tambah(4,6))
+
+
+# def tambah(a,b):
+#     hasil = a+b
+#     return 2
+
+# tambahan = tambah(2,4) + tambah (4,6)
+
+# # print(tambahan)
+
+# def pilihan (opsi):
+#     opsi = input("Masukkan pilihan")
+#     match opsi:
+#         case 1:
+#             print(1)
+#             return
+#         case 2:
+#             print(2)
+#             return
+#     print(3)
+
+# pilihan(2)
+
+# def cetak (nama, nim, **matkul):
+#     print(nama, nim, matkul)
+
+# cetak ("Fathir", 41, matematika=90, fisika=80, kimia=70)
+
+# var=2,4,5
+
+# var1,var2,var3=var
+
+# print(var)
+# print(var1)
+# print(var2)
+# print(var3)
+
+# import os
+# os.system("cls")
+
+# Nama = "Informatika"
+# Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+# kelas = "IT Asikk"
+
+# def info():
+#     Nama = "Teknik Elektro"
+#     Mata_Kuliah = "Pengantar Teknik ELektro"
+#     print("Prodi:", Nama)
+#     print("Mata Kuliah:", Mata_Kuliah)
+#     print(kelas)
+#     def dalamdef(var):
+#         print(var)
+# info()
+# print("Prodi:", Nama)
+# print("Mata Kuliah:", Mata_Kuliah)
+
+# # dalamdef(10)
+
+# def cetak(nama, nim, umur=18):
+#     print (nama,nim,umur)
+
+# cetak("fathir",41,112)
+
+
+
+
+
